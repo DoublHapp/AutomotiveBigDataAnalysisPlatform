364b9596c70fca01c458be3fbc84f8be7a504deb
DBFFFF <3500242294@qq.com>
2025-06-24 10:07:33 +0800

    init

---
+++ ABDAP_BackEnd/src/main/java/cn/com/undefined/abdap_backend/AbdapBackEndApplication.java
@@ -0,0 +1,13 @@
+package cn.com.undefined.abdap_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbdapBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbdapBackEndApplication.class, args);
+    }
+
+}
