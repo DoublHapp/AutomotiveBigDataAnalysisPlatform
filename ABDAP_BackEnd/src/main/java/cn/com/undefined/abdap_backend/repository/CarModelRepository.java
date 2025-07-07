4ed6cf8e80c63e0ea51042a77f03d69ca55f0ec9
AP <2507781170@qq.com>
2025-07-07 17:05:07 +0800

    try fix N+1

--- ABDAP_BackEnd/src/main/java/cn/com/undefined/abdap_backend/repository/CarModelRepository.java
+++ ABDAP_BackEnd/src/main/java/cn/com/undefined/abdap_backend/repository/CarModelRepository.java
@@ -1,7 +1,12 @@
 package cn.com.undefined.abdap_backend.repository;
 
+import cn.com.undefined.abdap_backend.dto.CarModelDTO;
 import cn.com.undefined.abdap_backend.entity.CarModel;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,5 +14,10 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface CarModelRepository extends JpaRepository<CarModel, Long> {
-    
+    @Query("SELECT new cn.com.undefined.abdap_backend.dto.CarModelDTO(" +
+            "c.carModelId, c.modelName, c.modelFullName, c.brandId, " +
+            "b.brandName, c.level, c.launchDate, c.officialPrice, " +
+            "c.engineType, c.seatNum, c.driveType, c.rangeKm, c.imageUrl) " +
+            "FROM CarModel c LEFT JOIN c.brand b")
+    List<CarModelDTO> findAllCarModelDTOs();
 }
