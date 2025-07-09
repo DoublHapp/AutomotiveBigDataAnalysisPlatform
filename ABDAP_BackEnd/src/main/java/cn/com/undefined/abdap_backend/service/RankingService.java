148b0705262f01e64bf7c021b9f0630435a079cb
AP <2507781170@qq.com>
2025-07-09 21:56:21 +0800

    fix: 为rank_type添加唯一约束

--- ABDAP_BackEnd/src/main/java/cn/com/undefined/abdap_backend/service/RankingService.java
+++ ABDAP_BackEnd/src/main/java/cn/com/undefined/abdap_backend/service/RankingService.java
@@ -12,6 +12,7 @@ import cn.com.undefined.abdap_backend.entity.Ranking;
 import cn.com.undefined.abdap_backend.repository.RankingRepository;
 import cn.com.undefined.abdap_backend.repository.SaleRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -484,6 +485,10 @@ public class RankingService {
                         // 处理序列化异常
                         e.printStackTrace();
                         System.err.println("\n\n\nError saving ranking data: " + e.getMessage() + "\n\n\n");
+                } catch(DataIntegrityViolationException e){
+                        // 处理数据完整性异常（如唯一约束冲突）
+                        e.printStackTrace();
+                        System.err.println("\n\n\nData integrity violation: " + e.getMessage() + "\n\n\n");
                 }
         }
 
